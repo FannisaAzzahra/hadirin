f833b60f34ea48af3a8e29aa79579a178cbc411f
FannisaAzzahra <fannisaazzahra1414@gmail.com>
2025-07-16 10:37:12 +0700

    Clean

--- resources/views/pages/presence/index.blade.php
+++ resources/views/pages/presence/index.blade.php
@@ -19,59 +19,11 @@
                     
             </div>
             <div class="card-body">
-                {{-- <table id="datatable" class="table table-striped">
-                    <thead>
-                        <tr>
-                           <th>No.</th>
-                           <th>Nama Kegiatan</th>
-                           <th>Tanggal Kegiatan</th>
-                           <th>Waktu Mulai</th>
-                           <th>Opsi</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @if ($presences->isEmpty())
-                            <tr>
-                                <td colspan="5" class="text-center">Tidak ada data</td>
-                            </tr>
-                        @endif
-                        @foreach ($presences as $presence )
-                            <tr>
-                                <td>{{ $loop->iteration }}</td>
-                                <td>{{ $presence->nama_kegiatan }}</td>
-                                <td>{{ date('d-m-Y', strtotime($presence->tgl_kegiatan)) }}</td>
-                                <td>{{ date('H:i', strtotime($presence->tgl_kegiatan)) }}</td>
-                                <td>
-                                    <a href="{{ route('presence.show', $presence->id) }}" class="btn btn-secondary">
-                                        Detail
-                                    </a>
-                                    <a href="{{ route('presence.edit', $presence->id) }}" class="btn btn-warning">
-                                        Edit
-                                    </a>
-                                    <form action="{{ route('presence.destroy', $presence->id) }}" method="post" class="d-inline">
-                                        @csrf
-                                        @method('delete')
-                                        <button type="submit" class="btn btn-danger" onclick="return confirm ('Apakah Anda yakin ingin menghapus data ini?')">
-                                            Hapus
-                                        </button>
-                                    </form>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table> --}}
-
                 {{ $dataTable->table() }}
             </div>
         </div>
     </div>
 @endsection
-
-{{-- @push('js')
-    <script>
-        new DataTable('#datatable');
-    </script>
-@endpush --}}
 
 @push('js')
     {{ $dataTable->scripts(attributes: ['type' => 'module']) }}
